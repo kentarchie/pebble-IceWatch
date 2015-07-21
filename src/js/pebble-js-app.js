d28f4c072421290638a2e83bef273eead097584a
kent archie <kentarchie@gmail.com>
2015-07-20 22:03:46 -0500

    Added configuration management, not complete

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,24 @@
+var initialized = false;
+var options = {};
+
+Pebble.addEventListener("ready", function() {
+  console.log("ready called!");
+  initialized = true;
+});
+
+Pebble.addEventListener("showConfiguration", function() {
+  console.log("showing configuration");
+  Pebble.openURL('http://archie-perkins.com/pebble/IceWatchConfig.html?'+encodeURIComponent(JSON.stringify(options)));
+});
+
+Pebble.addEventListener("webviewclosed", function(e) {
+  console.log("configuration closed");
+  // webview closed
+  //Using primitive JSON validity and non-empty check
+  if (e.response.charAt(0) == "{" && e.response.slice(-1) == "}" && e.response.length > 5) {
+    options = JSON.parse(decodeURIComponent(e.response));
+    console.log("Options = " + JSON.stringify(options));
+  } else {
+    console.log("Cancelled");
+  }
+});
--- tabs.vim
+++ tabs.vim
@@ -1,2 +1,4 @@
 tabe ./src/IceWatch.c
+tabe IceWatchConfig.html
+tabe ./src/js/pebble-js-app.js
 tabe ./wscript
